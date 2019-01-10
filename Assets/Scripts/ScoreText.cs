f275b628c586a8023a612f6f351afd0f4c51a6a9
ortofone24 <ortofone24@gmail.com>
2019-01-10 04:05:29 +0100

    second commit

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    private Text score;
+
+    private void Start()
+    {
+        score = GetComponent<Text>();
+        score.text = "Score: " + GameManager.Instance.Score;
+    }
+
+}
